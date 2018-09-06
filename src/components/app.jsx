36dba2f7abf4a34cab86dbb6b99020e5a4f33523
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-09-06 14:30:09 -0300

    working gif search

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api'
+
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx'
+import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      gifs: [],
+      selectedGifId: null
+    }
+  }
+
+  search = (query) => {
+    giphy("9J8x1QSfzKgl5SRINng9gnHrlOGtDh8j").search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, results) => {
+      this.setState({
+        gifs: results.data
+      })
+    });
+  }
+
+  gifClicked = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <div className="gif-list">
-
-          </div>
-
+            <GifList gifs={this.state.gifs} selectGif={this.gifClicked} />
         </div>
       </div>
 
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -3,10 +3,18 @@ import React, { Component } from 'react'
 
 class Gif extends Component {
 
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id)
+    }
+  }
+
   render() {
-    const src = "https://media.giphy.com/media/3oEhmGTWHYoWxAENZm/giphy.gif";
+
+    const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`;
+
     return (
-      <img src={src} alt="" className="gif" />
+      <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
     );
   }
 }
